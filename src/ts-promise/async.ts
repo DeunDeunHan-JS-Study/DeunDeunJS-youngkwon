91769991e24615a72c933abcb287f7e016507ca7
youngkwon02 <youngkwon02@naver.com>
2022-05-15 18:18:06 +0900

    feat: ts-promise/async/order func

--- src/ts-promise/async.ts
+++ src/ts-promise/async.ts
@@ -1,3 +1,12 @@
 const restaurant = (callback: () => void, time: number) => {
   setTimeout(callback, time);
 };
+
+const order = (): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    restaurant(() => {
+      console.log("[레스토랑 진행상황 - 음식 주문]");
+      resolve("음식 주문 시작");
+    }, 1000);
+  });
+};
